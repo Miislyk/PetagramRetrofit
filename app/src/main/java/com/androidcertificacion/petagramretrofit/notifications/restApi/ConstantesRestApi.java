834d0d12a8105dc5030b1f817a4ca176a5e197b4
Miislyk <gac.luish@gmail.com>
2017-02-14 12:43:23 -0600

    PetagramNotifications

---
+++ app/src/main/java/com/androidcertificacion/petagramretrofit/notifications/restApi/ConstantesRestApi.java
@@ -0,0 +1,12 @@
+package com.androidcertificacion.petagramretrofit.notifications.restApi;
+
+/**
+ * Created by lhrat on 07/02/2017.
+ */
+
+public final class ConstantesRestApi {
+
+    public static final String ROOT_URL = "https://glacial-brushlands-83248.herokuapp.com/";
+    public static final String ENDPOINT = "registrar-usuario/";
+
+}
